04a56f3c670379e5404176adcb0a9cee78f3543e
gvera <gvera>
2017-03-27 18:16:17 -0300

    mejora hasta 89

--- src/tddTest/ArabicToRomansConverterTest.java
+++ src/tddTest/ArabicToRomansConverterTest.java
@@ -38,58 +38,98 @@ public class ArabicToRomansConverterTest {
 	public void test8() {
 		assertEquals("VIII", convert(8));
 	}
+	@Test
 	public void test9() {
 		assertEquals("IX", convert(9));
 	}
+	@Test
 	public void test10() {
 		assertEquals("X", convert(10));
 	}
+	@Test
 	public void test11() {
 		assertEquals("XI", convert(11));
 	}
+	@Test
 	public void test12() {
 		assertEquals("XII", convert(12));
 	}
+	@Test
 	public void test15() {
 		assertEquals("XV", convert(15));
 	}
+	@Test
 	public void test16() {
 		assertEquals("XVI", convert(16));
+	}
+	@Test
+	public void test19() {
+		assertEquals("XIX", convert(19));
+	}
+	@Test
+	public void test20() {
+		assertEquals("XX", convert(20));
+	}
+	@Test
+	public void test23() {
+		assertEquals("XXIII", convert(23));
+	}
+	@Test
+	public void test35() {
+		assertEquals("XXXV", convert(35));
+	}
+	@Test
+	public void test45() {
+		assertEquals("XLV", convert(45));
+	}
+	@Test
+	public void test65() {
+		assertEquals("LXV", convert(65));
 	}
 
 
 	private String convert(int i) {
 			String s = "";
-			Integer j;
+			
 			do {
-				if (i < 10) {
-					if (i>=1 && i<4) {
-						for (j=0;j<i;j++){
-							s = s.concat("I");
-						}
-					}
-					else if (i==4)
-						s = "IV";
-					else if (i>=5 && i<9) {
-						s = "V";
-						for (j=5;j<i;j++){
-							s = s.concat("I");
-						}
-					}
-					else if (i==9)
-						s = "IX";
-					else if (i==10)
-						s = "X";
-					i = 0;
+				if (i>=50) {
+					s = s.concat("L");
+					i = i - 50;
 				}
-				else if (i>10) {
-					s = "X";
+				if (i>=40) {
+					s = s.concat("XL");
+					i = i - 40;
+				}
+				if (i>=10) {
+					s = s.concat("X");
 					i = i - 10;
 				}
+				else if (i < 10) {
+					if (i>=1 && i<4) {
+						s = ciclo(0, i, s);
+					}
+					else if (i>=4 && i<9) {
+						if (i==4) {
+							s = s.concat("I");
+						}
+						s = s.concat("V");
+						s = ciclo(5, i, s);
+					}
+					else if (i==9)
+						s = s.concat("IX");
+					i = 0;
+				}
+				
 			} while (i>0);
-
-			//ejemplo6
 			return s;
+	}
+	
+	private String ciclo(int inicial, int i, String s) {
+		int j;
+		for (j=inicial;j<i;j++){
+			s = s.concat("I");
+		}
+		return s;
 	}
 
 }
